4e89acac4606e64a2c1b5a271b3c55bbe6c71d0e
Utku SENOCAK <u.senocakk@gmail.com>
2021-09-01 23:29:32 +0300

    Add example of unit programming

---
+++ UnitTestExample/unit.cpp
@@ -0,0 +1,20 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+int fibonacci(int n) {
+	if (n <= 1) {
+		return n;
+	}
+	return (fibonacci(n - 1) + fibonacci(n - 2));
+}
+
+TEST_CASE("Fibonacci series", "[fibonacci]") {
+	REQUIRE(fibonacci(0) == 0);
+	REQUIRE(fibonacci(1) == 1);
+	REQUIRE(fibonacci(2) == 1);
+	REQUIRE(fibonacci(3) == 2);
+	REQUIRE(fibonacci(4) == 3);
+	REQUIRE(fibonacci(5) == 5);
+	REQUIRE(fibonacci(12) == 144);
+
+}
