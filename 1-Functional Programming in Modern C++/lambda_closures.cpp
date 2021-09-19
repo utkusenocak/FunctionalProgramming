443eeb0580925e6a797f0a3d48068fab0e27ea42
Utku SENOCAK <u.senocakk@gmail.com>
2021-09-19 19:13:34 +0300

    Add example of lambda closure functions

---
+++ 1-Functional Programming in Modern C++/lambda_closures.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+auto incrementer = []() {
+	auto increment = 0;
+	return [increment]() mutable {
+		return increment++;
+	};
+};
+
+int main() {
+	auto myInc = incrementer();
+	cout << "myInc = " << myInc() << endl;
+	cout << "myInc = " << myInc() << endl;
+	cout << "myInc = " << myInc() << endl;
+	cout << "myInc = " << myInc() << endl;
+	return 0;
+}
